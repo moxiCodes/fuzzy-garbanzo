d04203d859271eaceb414c082b01b9daac468af4
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-06-26 14:39:48 +0100

    Fixed typo

--- my-app-ts/src/components/Alert/Alert.tsx
+++ my-app-ts/src/components/Alert/Alert.tsx
@@ -9,5 +9,5 @@ type AlertProps = {
 };
 
 export const Alert = ({ message, type = "info" }: AlertProps) => (
-  <div className={classNames(styles.alertage, styles[type])}>{message}</div>
+  <div className={classNames(styles.alert, styles[type])}>{message}</div>
 );
