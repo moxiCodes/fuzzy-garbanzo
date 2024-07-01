2fa4e332de13556927ad6c085ad7f0c16a99d162
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-07-01 15:46:55 +0100

    Fix for broken git pull

--- my-app-ts/src/App.tsx
+++ my-app-ts/src/App.tsx
@@ -1,30 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { EmployeeBadge } from "./v5-employee-task/solution/EmployeeBadge";
-import { Employee } from "./v5-employee-task/Employee";
 
 function App() {
-  // Probably from an API
-
-  const employee: Employee = {
-    details: {
-      firstName: "Tinky",
-      lastName: "Winky",
-    },
-    email: "tiny.winky@teletubbies.com",
-    imagePath: "tinky-winky.jpg",
-    jobTitle: "Teletubby",
-    team: "Client Servicing and Engagement",
-    cohort: 12,
-  };
-
-  console.log("employee", employee);
-
   return (
     <div className="App">
       <header className="App-header">
-        <EmployeeBadge {...employee} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
