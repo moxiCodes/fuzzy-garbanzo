712be646c9ed799e272b88744725f5f3e7b7ae40
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-07-04 10:24:40 +0100

    Updated players api

--- players-api/src/index.ts
+++ players-api/src/index.ts
@@ -3,10 +3,12 @@ import express from "express";
 const app = express();
 const port = 4000;
 
-app.get("/players", (req, res) => {
+app.get("/players", async (req, res) => {
+  await sleep(1500);
   res.header("Access-Control-Allow-Origin", "*");
   res.send([
     {
+      id: 1,
       firstName: "Sean",
       lastName: "Abbott",
       team: "Australia",
@@ -14,6 +16,7 @@ app.get("/players", (req, res) => {
       bowlingAverage: 30.93,
     },
     {
+      id: 2,
       firstName: "Pat",
       lastName: "Cummins",
       team: "Australia",
@@ -21,9 +24,18 @@ app.get("/players", (req, res) => {
       bowlingAverage: 23.22,
     },
     {
+      id: 3,
       firstName: "Moeen",
       lastName: "Ali",
       team: "England",
+      battingAverage: 28.12,
+      bowlingAverage: 37.31,
+    },
+    {
+      id: 4,
+      firstName: "Mitchell",
+      lastName: "Marsh",
+      team: "Australia",
       battingAverage: 28.12,
       bowlingAverage: 37.31,
     },
@@ -33,3 +45,6 @@ app.get("/players", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+const sleep = (milliseconds: number) =>
+  new Promise((resolve) => setTimeout(resolve, milliseconds));
