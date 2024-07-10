92f3731d34ca1351d8f45815c856a0b922b135ae
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-07-10 09:24:24 +0100

    Fixed useEmployeesFixed

---
+++ alert-stack/src/components/solution/Employee/useEmployeesFixed.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Employee } from "./Employee";
+
+export const useEmployees = (searchText: string) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [employees, setEmployees] = useState<Employee[]>([]);
+
+  useEffect(() => {
+    const getEmployees = async () => {
+      setIsLoading(true);
+      const { data } = await axios.get<Employee[]>("/employees", {
+        params: { search: searchText },
+      });
+      setEmployees(data);
+      setIsLoading(false);
+    };
+    getEmployees();
+  }, [searchText]);
+
+  return { isLoading, employees };
+};
