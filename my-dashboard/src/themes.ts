16913c4f36fd6842f26199b15373495bcee33cad
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-07-19 11:27:54 +0100

    Dashboard initial scaffolding

---
+++ my-dashboard/src/themes.ts
@@ -0,0 +1,22 @@
+export const light = {
+  primary: "#4851f4",
+  background: "#ffffff",
+  nav: "#f8f8f8",
+  border: "#deebf1",
+  text: "#202224",
+};
+export const dark = {
+  primary: "#4851f4",
+  background: "#1f2023",
+  nav: "#27282b",
+  border: "#303236",
+  text: "#f8f8f8",
+};
+
+export const birindar = {
+  primary: "purple",
+  background: "yellow",
+  nav: "pink",
+  border: "darkpurple",
+  text: "deeppink",
+};
