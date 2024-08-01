755decda2bd849c4716d2bcb9f04291f7b458901
DuncanEdwards <duncan.edwards@abide-financial.com>
2024-08-01 17:27:29 +0100

    Added missing file

---
+++ chef-selector/src/live-solution/Table/StyledTableContainer.styled.tsx
@@ -0,0 +1,7 @@
+import { TableContainer } from "@mui/material";
+import styled from "styled-components";
+
+export const StyledTableContainer = styled(TableContainer)`
+  border: solid 1px gray;
+  max-height: 500px;
+`;
